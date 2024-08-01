c63e142d5936e78c72be0e6772d8cd8f696b37ef
saketjh13 <jsaket628@gmail.com>
2024-08-01 21:07:28 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import { Navigate, Route, Routes } from 'react-router-dom'
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Navigate t0="/login" /> } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
